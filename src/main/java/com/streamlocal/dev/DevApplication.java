7d32294de1186b9afb4cd2872ab3f1b54f350ae0
JaiswalShubham-1 <iamshubham1111998@gmail.com>
2026-05-14 13:33:32 +0530

    Week 1: security filter + infra (password via env vars)

--- src/main/java/com/streamlocal/dev/DevApplication.java
+++ src/main/java/com/streamlocal/dev/DevApplication.java
@@ -2,12 +2,12 @@ package com.streamlocal.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class DevApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DevApplication.class, args);
+    }
 }
